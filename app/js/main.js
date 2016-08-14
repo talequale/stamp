eca62a5077ad8797b893d84b2130ac890e52e499
Kirill Demidov <kademidov@gmail.com>
2016-08-14 12:44:42 +0300

    v0.1.1 in work

--- app/js/main.js
+++ app/js/main.js
@@ -6,7 +6,37 @@ $(document).ready(function(){
     margin: 0,
     loop: true,
     nav: true,
-    navText: false
+    navText: false,
+    responsiveClass:true,
+    responsive : {
+    // breakpoint from 0 up
+      0 : {
+          items : 1,
+      },
+      // breakpoint from 1028 up
+      1028 : {
+          items : 4,
+      }
+    }
+  });
+
+  // var top = $('.promo__sloganbox').offset().top;
+  // console.log(top);
+
+  $(function () {
+
+    $(window).scroll(function () {
+
+      var top = $('.promo__sloganbox').offset().top;
+      console.log(top);
+      
+      if ($(this).scrollTop() > top) {
+        $('.page-header').addClass('page-header--scroll');
+      } else {
+        $('.page-header').removeClass('page-header--scroll');
+      }
+    });
+
   });
 
 });
