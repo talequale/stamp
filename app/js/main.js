627d17765bb7efdd52687dd31cd0d094a4f4fabb
Nikolay Gromov <nicothin@gmail.com>
2016-08-17 20:45:43 +0300

    slider width & num ect.

--- app/js/main.js
+++ app/js/main.js
@@ -4,7 +4,8 @@ $(document).ready(function(){
     center: true,
     items: 4,
     margin: 0,
-    loop: true,
+    // loop: true,
+    startPosition: 3, // только если слайдов более 3-х изначально
     nav: true,
     navText: false,
     responsiveClass:true,
@@ -16,6 +17,15 @@ $(document).ready(function(){
       // breakpoint from 1028 up
       1028 : {
           items : 4,
+      },
+      1200 : {
+          items : 5,
+      },
+      1600 : {
+          items : 6,
+      },
+      1920 : {
+          items : 7,
       }
     }
   });
@@ -29,7 +39,7 @@ $(document).ready(function(){
 
       var top = $('.promo__sloganbox').offset().top;
       console.log(top);
-      
+
       if ($(this).scrollTop() > top) {
         $('.page-header').addClass('page-header--scroll');
       } else {
