5b47df565313ea9548eab624cb29bbfee2977dcc
denzakh <den.zakh@gmail.com>
2016-08-29 19:52:34 +0300

    верхняя плашка, дропареа

--- app/js/main.js
+++ app/js/main.js
@@ -63,14 +63,23 @@ $(document).ready(function(){
   //скрол верхней панели
   
   $(function () {
-
+    var choise = $('.order__rigging').offset().top - 180; 
     $(window).scroll(function () {
 
-      if ( ($(this).scrollTop() > 110) && ($(this).scrollTop() < 430) ) {
+      if ( $(this).scrollTop() > 110) {
         $('.decision-header').addClass('decision-header--scroll');
       } else {
         $('.decision-header').removeClass('decision-header--scroll');
       }
+
+      if (  $(this).scrollTop() > choise ) {
+        $('.decision-header').addClass('decision-header--stop');
+        $('.decision-header').css('top', choise);
+      } else {
+        $('.decision-header').removeClass('decision-header--stop');
+        $('.decision-header').css('top', '0');
+      }
+
     });
 
   });
